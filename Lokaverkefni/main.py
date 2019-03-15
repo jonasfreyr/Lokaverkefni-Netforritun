445938be44de8c78a0f8fb50a7a82460122724fb
jonasfreyr <jonasfreyrbjarnason@gmail.com>
2019-03-15 12:17:37 +0000

    Add files via upload

---
+++ Lokaverkefni/main.py
@@ -0,0 +1,25 @@
+from tkinter import *
+from settings import *
+
+root = Tk()
+
+class ChatApp:
+    def __init__(self):
+        root.geometry(str(WINDOW_SIZE["width"]) + "x" + str(WINDOW_SIZE["height"]))
+        root.resizable(width=False, height=False)
+
+    def clear_window(self):
+        for a in root.winfo_children():
+            a.destroy()
+
+    def make_window(self):
+        self.clear_window()
+
+        Label(root, text="Helo wrld!").pack()
+
+        Text(root, width = TEXT_BOX_SIZE["width"], height = TEXT_BOX_SIZE["height"]).pack()
+
+
+h = ChatApp()
+h.make_window()
+root.mainloop()
---
+++ Lokaverkefni/settings.py
@@ -0,0 +1,5 @@
+# Text box
+TEXT_BOX_SIZE = {"height": 5, "width": 20}
+
+# Window settings
+WINDOW_SIZE = {"height": 400, "width": 600}
