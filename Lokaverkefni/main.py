dae267d458166d99ab9056769cd604d94489461e
jonasfreyr <jonasfreyrbjarnason@gmail.com>
2019-03-18 19:18:41 +0000

    Add files via upload

--- Lokaverkefni/main.py
+++ Lokaverkefni/main.py
@@ -1,3 +1,4 @@
+'''
 from tkinter import *
 from settings import *
 
@@ -5,7 +6,7 @@ root = Tk()
 
 class ChatApp:
     def __init__(self):
-        root.geometry(str(WINDOW_SIZE["width"]) + "x" + str(WINDOW_SIZE["height"]))
+        root.geometry(str(WINDOW.width) + "x" + str(WINDOW.height))
         root.resizable(width=False, height=False)
 
     def clear_window(self):
@@ -16,11 +17,54 @@ class ChatApp:
         self.clear_window()
 
 
-        Label(root, text="Helo wrld!", bg="WHITE", width=TEXT_DISPLAY_SIZE["width"], height=TEXT_DISPLAY_SIZE["height"]).place(x=WINDOW_SIZE["width"]/2, y=WINDOW_SIZE["height"]/2, anchor=CENTER)
+        Label(root, text="Helo wrld!", bg="WHITE", width=TEXT_DISPLAY.width, height=TEXT_DISPLAY.height).place(x=TEXT_DISPLAY.pos.x, y=TEXT_DISPLAY.pos.y, anchor=CENTER)
 
-        Text(root, width = TEXT_BOX_SIZE["width"], height = TEXT_BOX_SIZE["height"]).place(x=WINDOW_SIZE["width"]/2, y=WINDOW_SIZE["height"]-20, anchor=CENTER)
+        Entry(root, width = TEXT_BOX.width).place(x=TEXT_BOX.pos.x, y=TEXT_BOX.pos.y, anchor=CENTER)
 
+        Button(root, text="Send").place(x=BUTTON.pos.x, y=BUTTON.pos.y, anchor=CENTER)
 
 h = ChatApp()
 h.make_window()
 root.mainloop()
+'''
+
+'''
+kivy
+kivy.deps.sdl2
+kivy.deps.glew
+kivy.deps.gstreamer
+kivy.deps.angle
+'''
+
+'''
+import kivy
+kivy.require("1.8.0")
+
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+
+
+class LoginScreen(GridLayout):
+    def __init__(self, **kwargs):
+        super(LoginScreen, self).__init__(**kwargs)
+        self.cols = 1
+
+        self.add_widget(Label(text="Shat"))
+        self.username = TextInput(multiline=False)
+        self.add_widget(self.username)
+        self.add_widget(Button(text="Send"))
+
+
+class ChatApp(App):
+    def build(self):
+        return LoginScreen()
+
+if __name__ == "__main__":
+    ChatApp().run()
+'''
+
+import site, sys, os
+print(os.path.dirname(sys.executable))
--- Lokaverkefni/settings.py
+++ Lokaverkefni/settings.py
@@ -1,12 +1,41 @@
-# Text box
-TEXT_BOX_SIZE = {"height": 1, "width": 20}
-
-# Text display
-TEXT_DISPLAY_SIZE = {"height": 22, "width": 40}
+import pygame as pg
+pg.init()
+vec = pg.math.Vector2
 
 # Window settings
-WINDOW_SIZE = {"height": 400, "width": 600}
+class Window:
+    def __init__(self):
+        self.height = 400
+        self.width = 600
+
+WINDOW = Window()
+
+# Text box
+class TextBox:
+    def __init__(self):
+        self.height = 1
+        self.width = 35
+
+        self.pos = vec(WINDOW.width / 2 - 20, WINDOW.height - 20)
+
+TEXT_BOX = TextBox()
+
+
+# Text display
+class TextDisplay:
+    def __init__(self):
+        self.height = 22
+        self.width = 40
+
+        self.pos = vec(WINDOW.width / 2, WINDOW.height / 2 - 20)
+
+TEXT_DISPLAY = TextDisplay()
+
+class ButtoN:
+    def __init__(self):
+        self.pos = vec(WINDOW.width / 2 + 110, WINDOW.height - 20)
+
+BUTTON = ButtoN()
 
 # Users
-
 USERS = ["Lalli", "Palli", "LEP"]
